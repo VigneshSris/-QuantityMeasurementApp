35eca2e30c99d81eb8b6e9ff4f4faca449afac79
vigneshsris <vigneshsri3535@gmail.com>
2026-04-27 14:28:07 +0530

    UC2: Add feet and inches measurement equality check

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,5 +1,6 @@
 public class QuantityMeasurementApp {
 
+    // Feet Class
     static class Feet {
         private final double value;
 
@@ -21,9 +22,40 @@ public class QuantityMeasurementApp {
         }
     }
 
+    // Inches Class
+    static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+
+    // Static Methods
+    public static boolean compareFeet(double v1, double v2) {
+        return new Feet(v1).equals(new Feet(v2));
+    }
+
+    public static boolean compareInches(double v1, double v2) {
+        return new Inches(v1).equals(new Inches(v2));
+    }
+
+    // Main Method
     public static void main(String[] args) {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
-        System.out.println(f1.equals(f2));
+        System.out.println("Feet Equality: " + compareFeet(1.0, 1.0));
+        System.out.println("Inches Equality: " + compareInches(1.0, 1.0));
     }
 }
